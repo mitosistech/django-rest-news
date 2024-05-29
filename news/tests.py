54cd24421fc9bbf598105dd5349989589e17de0c
mitosistech <sathiyan@intemo.tech>
2024-05-29 14:56:12 +0530

    app is added

--- news/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-def example_function(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
-
-parameters = {'param1': 'value1', 'param2': 'value2'}
-example_function(**parameters)
